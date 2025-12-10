913a7c92058140aca9cf7c836d5fa1f5a5693183
Sai <dineshsai18@gmail.com>
2025-12-10 15:50:25 +0530

    Initial Streamlit test app

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.title("EAWT – Certificate Generator (Public Demo)")
+st.write("If you see this text on Streamlit Cloud, deployment works.")
